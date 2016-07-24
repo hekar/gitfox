3b59d7d6ffe60aace45f7e1c78232f8b9d88030c
Hekar Khani <hekark@gmail.com>
2016-07-24 19:57:43 -0400

    Additional tests for merge

--- test/unit/command/pull/merge-test.js
+++ test/unit/command/pull/merge-test.js
@@ -1,16 +1,42 @@
 'use strict';
 
 /*global describe,it*/
+const _ = require('lodash');
 const src = '../../../../lib';
 const rewire = require('rewire');
 const merge = rewire(`${src}/command/pull/merge`);
 const chai = require('chai');
 
+function assertMerge(remote, local) {
+  chai.expect(remote)
+    .to.deep.equal(merge.copyRemote(remote, local).local);
+}
+
 describe('merge', () => {
   describe('copy-remote', () => {
     describe('#copyRemote(...)', () => {
-      it('should add additional folders', () => {
+      it('should not add existing nodes', () => {
+        const local = {
+          id: 0,
+          children: [{
+            id: 1,
+            children: [{
+              id: 2,
+              children: [{
+                id: 3,
+                children: []
+              }]
+            }, {
+              id: 4,
+              children: []
+            }]
+          }]
+        };
 
+        assertMerge(_.clone(local), local);
+      });
+
+      it('should add additional folders', () => {
         const local = {
           id: 0,
           children: [{
@@ -53,11 +79,69 @@ describe('merge', () => {
           }]
         };
 
-        const actual = merge.copyRemote(remote, local);
-        const expected = remote;
+        assertMerge(remote, local);
+      });
 
-        chai.expect(expected)
-          .to.deep.equal(actual.local);
+      it('should add when children field missing', () => {
+        const local = {
+          id: 0
+        };
+
+        const remote = {
+          id: 0,
+          children: [{
+            id: 1,
+            children: [{
+              id: 2,
+              children: [{
+                id: 3,
+                children: []
+              }]
+            }, {
+              id: 4,
+              children: []
+            }]
+          }]
+        };
+
+        assertMerge(remote, local);
+      });
+
+      it('should add when children field missing', () => {
+        const local = {
+          id: 0
+        };
+
+        const remote = {
+          id: 0,
+          children: [{
+            id: 1,
+            children: [{
+              id: 2,
+              children: [{
+                id: 3,
+                children: []
+              }]
+            }, {
+              id: 4,
+              children: []
+            }]
+          }]
+        };
+
+        assertMerge(remote, local);
+      });
+
+      it('should skip missing children on remote', () => {
+        const local = {
+          id: 0
+        };
+
+        const remote = {
+          id: 0
+        };
+
+        assertMerge(remote, local);
       });
     });
   });
