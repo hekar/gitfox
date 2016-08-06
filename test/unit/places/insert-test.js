7d8aa26804d547ef209a38672b8b8ded99f90688
Hekar Khani <hekark@gmail.com>
2016-08-06 14:09:11 -0400

    Further work on testing merge

--- test/unit/places/insert-test.js
+++ test/unit/places/insert-test.js
@@ -5,7 +5,7 @@ const src = '../../../lib';
 const co = require('co');
 const _ = require('lodash');
 const rewire = require('rewire');
-const chai = require('chai');
+const sinon = require('sinon');
 const insert = rewire(`${src}/places/insert`);
 
 insert.__set__('sqlite3', {
@@ -13,8 +13,9 @@ insert.__set__('sqlite3', {
     return {
       all: function(query, params, done) {
         const callback = _.isFunction(done) ? done : params;
-        console.log(params);
-        callback(null, []);
+        callback(null, [{
+          id: 0
+        }]);
       }
     };
   }
@@ -24,12 +25,39 @@ insert.__set__('findFile', function *() {
   return '';
 });
 
-describe('', () => {
+function filterTree(node, predicate) {
+  const children = [];
+  for (let child of node.children) {
+    if (predicate(child)) {
+      children.push(child);
+      filterTree(child, predicate);
+    }
+  }
+
+  node.children = children;
+  return node;
+}
+
+describe('places', () => {
   const cn = {};
-  describe('', () => {
+  describe('#insert(...)', () => {
     it.only('should correctly traverse tree', co.wrap(function*() {
-      const tree = require('../resources/bookmarks-tree.json');
-      yield insert(cn, tree, []);
+      const localTree = require('../resources/bookmarks-tree.json');
+      //
+      const excludeIds = [470, 710, 711, 712, 713, 714];
+      const copied = [];
+      filterTree(localTree, (node) => {
+        const include = !_.includes(excludeIds, node.id);
+        if (!include) {
+          copied.push(node);
+        }
+        return include;
+      });
+
+      const remoteTree = require('../resources/bookmarks-tree.json');
+      yield insert(cn, remoteTree, copied);
+
+
     }));
   });
 });
